75cdf29bd2a734708ec57b41c5007ea989f49db8
euoom <euoom.dev@gmail.com>
2023-01-01 01:52:14 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbl3G-keUbY4dp99WEunvR53lq7zdgzJo",
+    authDomain: "wtwitter-3126a.firebaseapp.com",
+    projectId: "wtwitter-3126a",
+    storageBucket: "wtwitter-3126a.appspot.com",
+    messagingSenderId: "644593977215",
+    appId: "1:644593977215:web:e4658d529a5baf8ca27c4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
