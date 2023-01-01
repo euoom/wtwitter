7a6ddbf2366f184712e78b2d8fd4d8f45b6ecbc7
euoom <euoom.dev@gmail.com>
2023-01-01 19:33:29 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,28 +1,42 @@
 import React, {useState} from "react";
+import {authService} from "../fBase";
 
 const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [newAccount, setNewAccount] = useState(false);
 
     const onChange = (event) => {
-        const {target: {name, value}} = event ;
-        if (name==="email"){
+        const {target: {name, value}} = event;
+        if (name === "email") {
             setEmail(value);
-        } else if(name ==="password"){
+        } else if (name === "password") {
             setPassword(value)
         }
     };
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
-                <input name="email" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value="Log In"/>
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
+                <input name="password" type="password" placeholder="Password" required value={password}
+                       onChange={onChange}/>
+                <input type="submit" value={newAccount ? "create account" : "log in"}/>
             </form>
             <div>
                 <button>Continue with Google</button>
