4bb692f33e1ead5d0c52055e3495e56d670fccf6
euoom <euoom.dev@gmail.com>
2023-01-01 19:44:56 +0900

    #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,7 +4,7 @@ import {authService} from "../fBase";
 const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [newAccount, setNewAccount] = useState(false);
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) => {
         const {target: {name, value}} = event;
@@ -36,7 +36,7 @@ const Auth = () => {
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password}
                        onChange={onChange}/>
-                <input type="submit" value={newAccount ? "create account" : "log in"}/>
+                <input type="submit" value={newAccount ? "Create account" : "Log in"}/>
             </form>
             <div>
                 <button>Continue with Google</button>
